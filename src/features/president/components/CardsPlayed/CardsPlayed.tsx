b41243a3e6b2508d51c70f4d4c50b6c922974ead
tom <thomas.munro@btinternet.com>
2020-03-29 06:49:33 +0100

    💄 fix animation in

--- src/features/president/components/CardsPlayed/CardsPlayed.tsx
+++ src/features/president/components/CardsPlayed/CardsPlayed.tsx
@@ -21,10 +21,7 @@ export default function CardsPlayed() {
       {cardsPlayed.map((card, index) => (
         <div
           key={`${card}-${index}`}
-          style={{
-            position: "absolute",
-            transform: `rotate(${index * 30}deg)`
-          }}
+          style={{ ...getStyle(index), position: "absolute" }}
           className={styles.cardWrapper}
         >
           <Card label={card} className={styles.card} />
@@ -32,4 +29,39 @@ export default function CardsPlayed() {
       ))}
     </div>
   );
+}
+
+function getStyle(index: number) {
+  const midPoint = (300 - index * 30) / 2;
+  const animateIn = `@keyframes animatein-${index} {
+    0% {
+      opacity: 0;
+      transform: translateX(1000px) rotate(300deg);
+    }
+    50%{
+      opacity: 1;
+      transform: translateX(500px) rotate(${midPoint}deg);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0px) rotate(${index * 30}deg);
+    }
+  }
+  `;
+
+  const styleElement = document.createElement("style");
+  let styleSheet = null;
+
+  document.head.appendChild(styleElement);
+
+  styleSheet = styleElement.sheet;
+  // @ts-ignore
+  styleSheet.insertRule(animateIn, styleSheet.cssRules.length);
+
+  return {
+    transform: `rotate(${index * 30}deg)`,
+    animationName: `animatein-${index}`,
+    animationIterationCount: 1,
+    animationTimingFunction: "linear"
+  };
 }
